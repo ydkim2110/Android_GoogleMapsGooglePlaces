03d45820d553b3508440eb8a05c8af0e442a75ad
ydkim2110 <ydkim2110@gmail.com>
2018-11-25 17:04:34 +0900

    Google Maps & Google Places

---
+++ app/src/main/java/com/example/anti2110/googlemapsgoogleplaces/CustomInfoWindowAdapter.java
@@ -0,0 +1,54 @@
+package com.example.anti2110.googlemapsgoogleplaces;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by Kim Yongdae on 2018-11-25
+ * email : ydkim2110@gmail.com
+ */
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private static final String TAG = "CustomInfoWindowAdapter";
+
+    private final View mWindow;
+    private Context mContext;
+
+    public CustomInfoWindowAdapter(Context context) {
+        mContext = context;
+        mWindow = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null);
+    }
+
+    private void rendowWindowText(Marker marker, View view) {
+        String title = marker.getTitle();
+        TextView tvTitle = view.findViewById(R.id.title);
+
+        if (!title.equals("")) {
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = view.findViewById(R.id.snippet);
+
+        if (!snippet.equals("")) {
+            tvSnippet.setText(snippet);
+        }
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+}
